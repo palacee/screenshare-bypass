8ddee1fcc931b3fa4d32fbe11db985a5cc640d7a
hAxiT <ci1899084@gmail.com>
2022-04-10 18:04:13 +0200

    BD plugin for high quality screenshare

---
+++ ScreenShareBypass.plugin.js
@@ -0,0 +1,113 @@
+/**
+ * @name ScreenShareBypass
+ * @author palacee
+ * @authorId 737323631117598811
+ * @version 1.0
+ * @description Enable high quality screen sharing without Nitro
+ * @website https://osumatrix.me
+ * @source https://github.com/oSumAtrIX/BetterDiscordPlugins
+ * @updateUrl https://raw.githubusercontent.com/oSumAtrIX/BetterDiscordPlugins/master/ScreenShareBypass.plugin.js
+ */
+module.exports = (() => {
+	const config = {
+		info: {
+			name: "ScreenShareBypass",
+			authors: [
+				{
+					name: "palacee",
+					discord_id: "737323631117598811",
+					github_username: "palacee",
+				},
+			],
+			version: "1.0",
+			description:
+				"Enable high quality screen sharing without Nitro; Edited version of oSumAtrIX's BD Plugin",
+			github: "https://github.com/palacee",
+			github_raw:
+				"https://raw.githubusercontent.com/oSumAtrIX/BetterDiscordPlugins/master/ScreenShareBypass.plugin.js",
+		},
+		main: "ScreenShareBypass.plugin.js",
+	};
+
+	return !global.ZeresPluginLibrary
+		? class {
+				constructor() {
+					this._config = config;
+				}
+				getName() {
+					return config.info.name;
+				}
+				getAuthor() {
+					return config.info.authors.map((a) => a.name).join(", ");
+				}
+				getDescription() {
+					return config.info.description;
+				}
+				getVersion() {
+					return config.info.version;
+				}
+				load() {
+					BdApi.showConfirmationModal(
+						"Library Missing",
+						`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+						{
+							confirmText: "Download Now",
+							cancelText: "Cancel",
+							onConfirm: () => {
+								require("request").get(
+									"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+									async (error, response, body) => {
+										if (error)
+											return require("electron").shell.openExternal(
+												"https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
+											);
+										await new Promise((r) =>
+											require("fs").writeFile(
+												require("path").join(
+													BdApi.Plugins.folder,
+													"0PluginLibrary.plugin.js"
+												),
+												body,
+												r
+											)
+										);
+									}
+								);
+							},
+						}
+					);
+				}
+				start() {}
+				stop() {}
+		  }
+		: (([Plugin, Api]) => {
+				const plugin = (Plugin, Api) => {
+					const { Patcher, DiscordModules, Settings, PluginUtilities } = Api;
+					return class ScreenShareBypass extends Plugin {
+						settings = PluginUtilities.loadSettings(this.getName(), {
+							size: 48,
+						});
+
+						onStart() {
+							// spoof client side premium
+							let tries = 1;
+							let intervalId = setInterval(() => {
+								if (++tries > 5) clearInterval(intervalId);
+
+								const user =
+									ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser();
+								if (!user) return;
+								user.premiumType = 2;
+								clearInterval(intervalId);
+							}, 1000);
+						}
+
+						onStop() {
+							Patcher.unpatchAll();
+						}
+					};
+				};
+				return plugin(Plugin, Api);
+		  })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+/*@end@*/
